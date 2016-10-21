da12b7392dd6f90855edaac617170c518b2fa946
Adriel Fabella <adriel.fab@gmail.com>
2016-10-21 15:43:55 -0400

    Files updated
    According to the new folder heirarchy

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="main.css">
+		<!-- Custom CSS -->
+        <link rel="stylesheet" type="text/css" href="/assets/css/main.css">
+		
+        <!--Bootstrap-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		
     </head>
     <body>
         <div id="banner"></div>
@@ -47,5 +52,12 @@
         <footer>
             <p>All rights reserved</p>
         </footer>
+		
+
+		<!--JQuery-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<!--Bootstrap js-->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>		
+
     </body>
 </html>
