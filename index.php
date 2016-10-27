2a8eb5b0a3e6fd3a0d56ed5467550d2ff4c39377
Adriel Fabella <adriel.fab@gmail.com>
2016-10-27 03:55:07 -0400

    Created the calendar table and buttons

--- index.php
+++ index.php
@@ -18,42 +18,49 @@
         <div id="banner">Booking System</div>
 
         <?php
-            $date = date("Y/m/d");
+            date_default_timezone_set('America/New_York'); //Eastern time
+            $date= date_create('now'); //Creation of the date object, 'now' stands for current date
+            //example of adding days to the current date -> date_add($date,date_interval_create_from_date_string("40 days"));
+            $rooms = array("H908", "H432", "H843", "H123", "H732", "H320");
+            $bool_test = true; //A testing variable
         ?>
 
-        <div class="container">
-            <h3>Availabilities</h3>
-            <table style="width:80%">
-                <h4><?php echo $date ?></h4>
-
+        <div class="container" style="margin-top:40px;">
+            <a id="prev_date" href="index.php?url=prev_date"><i class="fa fa-arrow-left" aria-hidden="true">Previous date</i></a>
+            <a id="next_date" href="index.php?url=next_date"><i class="fa fa-arrow-right" aria-hidden="true" style="float:right">Next date</i></a>
+            <h4 id="date"><?php echo date_format($date,"Y-m-d"); ?></h4>
+            <table style="width:80%" align="center">
                 <tr>
-                    <th></th>
+                    <th>Room Numbers</th>
                     <th>7 - 10</th>
                     <th>10 - 13</th>
                     <th>13 - 16</th>
                     <th>16 - 19</th>
                     <th>19 - 22</th>
                 </tr>
-                <tr>
-                    <td>H908</td>
-                    <td><?php echo '*'; ?></td>
-                </tr>
-                <tr>
-                    <td>H432</td>
-                </tr>
-                <tr>
-                    <td>H843</td>
-                </tr>
-                <tr>
-                    <td>H123</td>
-                </tr>
-                <tr>
-                    <td>H732</td>
-                </tr>
-                <tr>
-                    <td>H320</td>
-                </tr>
-            </table style="width:40%">
+                <?php
+                //Prints out each row : room and date of time slot
+                    foreach($rooms as $room){
+                        echo '<tr>';
+                        echo '<td>' . $room . '</td>';
+                        for($i = 0; $i < 5; $i++){
+                            if($bool_test){
+                                echo '<td></td>';
+                            }
+                        }
+                        echo '</tr>';
+                    }
+                ?>
+            </table>
+            <hr>
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                     <button class="btn btn-warning">Make a Reservation</button>
+                     <button class="btn btn-info">Modify a Reservation</button>
+                     <button class="btn btn-danger">Drop a Reservation</button>
+                </div>
+            </div>
+
         </div>
     </body>
 
