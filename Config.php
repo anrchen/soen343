f7346dc0a22fa122bcaf4e9aa93b35aaf120cb0d
RhythmSaif <rhythmsaif@gmail.com>
2016-11-01 16:47:20 -0400

    Database Configuration
    Setting up database to test log-in.

--- Config.php
+++ Config.php
@@ -1,7 +1,24 @@
 <?php
-	define('DB_SERVER', 'localhost:3036');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', '');
-	define('DB_DATABASE', 'account');
-	$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "account";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=account", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = CREATE TABLE account
+			(		
+			username varchar(255),
+			password varchar(255),
+			);
+		$sql .= SELECT * FROM account;		
+        $conn->exec($sql);
+    }
+    catch(PDOException $e)
+    {
+        echo "Connection failed: " . $e->getMessage();
+    }
+
+    $conn=null;
 ?>
