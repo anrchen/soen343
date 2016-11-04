8e5eb07a5d4abc59fd1385fde0184fd850d7bc3b
Adriel Fabella <adriel.fab@gmail.com>
2016-11-04 16:22:57 -0400

    Initial sql file completed (added 3users)

--- conference.sql
+++ conference.sql
@@ -4,3 +4,8 @@ CREATE TABLE login(
   username VARCHAR(255) NOT NULL PRIMARY KEY,
   password VARCHAR(255) NOT NULL
 );
+
+INSERT INTO login (username, password)
+    VALUES('chen','gobliinmaster420'),
+      ('slifer', 'skydragon123'),
+      ('charizard', 'flamethrower987');
