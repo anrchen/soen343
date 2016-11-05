95322752f4dd11a7cbbbb2b2ec19e4f2c79d059d
An Ran Chen <anr_chen@live.concordia.ca>
2016-11-05 11:48:26 -0400

    Adding SQL queries & relational schema
    Adding SQL queries & relational schema
    Modified few data type restrictions

--- Config.php
+++ Config.php
@@ -9,8 +9,6 @@
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sql = CREATE TABLE account
 			(		
-			username varchar(255),
-			password varchar(255),
 			);
 		$sql .= SELECT * FROM account;		
         $conn->exec($sql);
--- conference.sql
+++ conference.sql
@@ -1,11 +1,39 @@
 USE conference;
 
-CREATE TABLE login(
-  username VARCHAR(255) NOT NULL PRIMARY KEY,
-  password VARCHAR(255) NOT NULL
+CREATE TABLE Login(
+  userName VARCHAR(25) NOT NULL PRIMARY KEY,
+  passWord VARCHAR(25) NOT NULL
 );
 
-INSERT INTO login (username, password)
+/*Each Room could have many reservations: One-to-Many relation*/
+/*Each userName has at most one Reservation: One-to-One relation*/
+CREATE TABLE Reservation(
+  ReservationID VARCHAR(10) NOT NULL PRIMARY KEY,
+  roomNumber INT(5) NOT NULL FOREIGN KEY REFERENCES RoomCatalog(roomNumber),
+  userName VARCHAR(255) NOT NULL FOREIGN KEYR EFERENCES Login(username)
+);
+
+CREATE TABLE RoomCatalog(
+  roomNumber INT(5) NOT NULL FOREIGN KEY REFERENCES RoomCatalog(roomNumber),
+);
+
+/*We are not adding timeSlots/ReservationID here, because it will cause data redundancy
+with the Reservation data Table. */
+ */
+CREATE TABLE Room(
+  roomNumber INT(5) NOT NULL PRIMARY KEY;
+  /*capacity ... */
+);
+
+/*Each Rev*/
+CREATE TABLE TimeSlot(
+  StartTime VARCHAR(10) NOT NULL,
+  EndTime VARCHAR(10) NOT NULL,
+  Waitlists VARCHAR(255) NOT NULL,
+  ReservationID VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES Reservation(ReservationID)
+);
+
+INSERT INTO Login (username, password)
     VALUES('chen','gobliinmaster420'),
       ('slifer', 'skydragon123'),
       ('charizard', 'flamethrower987');
