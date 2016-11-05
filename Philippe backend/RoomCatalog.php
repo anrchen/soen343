8764781235bffd80c3a466ed25793106b9195ca9
PhilippeKuret <philippekuret@gmail.com>
2016-11-05 17:42:46 -0400

    Functions from class Model
    I added some functions that are from the class model.
    I added extensive description on how they should function

---
+++ Philippe backend/RoomCatalog.php
@@ -0,0 +1,16 @@
+<?php
+	Class RoomCatalog
+	{
+		/*private Room[] Rooms;
+
+		public function_contruct(Room [] Rooms)
+		{
+			this.Rooms = Rooms;
+		}
+
+		public function GetRooms()
+		{
+			return Rooms;
+		}
+	}
+?>
