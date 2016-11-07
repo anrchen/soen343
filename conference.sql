b1a9dc18c727b7098ffcaf46638655440badc3e6
Adriel Fabella <adriel.fab@gmail.com>
2016-11-07 16:40:20 -0500

    Update of SQL file

--- conference.sql
+++ conference.sql
@@ -9,45 +9,42 @@ CREATE TABLE Login(
 /*Each Room could have many reservations: One-to-Many relation*/
 /*Each userName has at most one Reservation: One-to-One relation*/
 CREATE TABLE Reservation(
-  ReservationID VARCHAR(10) NOT NULL,
-  roomNumber INT(5) NOT NULL,
-  userName VARCHAR(255) NOT NULL,
-  PRIMARY KEY(ReservationID),
-  FOREIGN KEY(roomNumber) REFERENCES RoomCatalog(roomNumber)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-  FOREIGN KEY(userName) REFERENCES Login(username)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+  id INT(10) NOT NULL,
+  roomID INT(10) NOT NULL,
+  loginID INT(10) NOT NULL,
+  FOREIGN KEY(roomID) REFERENCES room(roomNumber)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+  FOREIGN KEY(loginID) REFERENCES login(userName)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+  PRIMARY KEY(id)
 );
 
-CREATE TABLE RoomCatalog(
-  roomNumber INT(5) NOT NULL,
-  FOREIGN KEY(roomNumber) REFERENCES Room(roomNumber)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-);
+
 
 /*We are not adding timeSlots/ReservationID here, because it will cause data redundancy
 with the Reservation data Table. */
 
 CREATE TABLE Room(
-  roomNumber INT(5) NOT NULL,
+  roomNumber VARCHAR(5) NOT NULL,
   PRIMARY KEY (roomNumber)
-  /*capacity ... */
 );
 
 /*Each Rev*/
 CREATE TABLE TimeSlot(
+  id INT(10) NOT NULL AUTO_INCREMENT,
   StartTime VARCHAR(10) NOT NULL,
   EndTime VARCHAR(10) NOT NULL,
-  Waitlists VARCHAR(255) NOT NULL,
-  ReservationID VARCHAR(10) NOT NULL,
-  FOREIGN KEY(ReservationID) REFERENCES Reservation(ReservationID)
+  ReservationID int(10) NOT NULL,
+  PRIMARY KEY(id),
+  FOREIGN KEY(ReservationID) REFERENCES reservation(id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
 
+
+/**ALTER FUNCTIONS**/
 INSERT INTO Login (username, password)
     VALUES('chen','gobliinmaster420'),
       ('slifer', 'skydragon123'),
