cb979141221a911969daefe905badbfec1e67d67
Adriel Fabella <adriel.fab@gmail.com>
2016-11-09 19:53:53 -0500

    Small changes

--- objects/Room.php
+++ objects/Room.php
@@ -2,14 +2,11 @@
 
 class Room{
 
+    //the only attribute we need is the room number
     private $roomNumber;
-    private $time;
-    private $user;
 
-    public function __construct($roomNumber, $time, $user){
+    public function __construct($roomNumber){
         $this->roomNumber = $roomNumber;
-        $this->time = $time;
-        $this->user = $user;
     }
 
     public function display(){
