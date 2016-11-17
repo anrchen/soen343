798f686248df04489d5192fca7944f0ef1264117
An Ran Chen <ky_chen93@hotmail.com>
2016-11-17 13:31:00 -0500

    Fixed include directory error

--- objects/ReservationCatalog.php
+++ objects/ReservationCatalog.php
@@ -1,7 +1,7 @@
 <?php
 
-    include (__DIR__.'\Reservation.php');
-    include (__DIR__.'\TimeSlot.php');
+    include (__DIR__.'/Reservation.php');
+    include (__DIR__.'/TimeSlot.php');
 
     class ReservationCatalog{
         private $reservations = [];
